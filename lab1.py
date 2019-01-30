6c285a85cb91d24abe3da2ed3d74f7d562d6231a
alexmatthewduval <35778640+alexmatthewduval@users.noreply.github.com>
2019-01-30 14:48:03 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,11 @@
+""""
+practice with defining functions
+lab1py
+Alex Duval
+1-30-2019
+ """
+ def area_of_rectangle(width,length):
+        width = int(input("width"))
+        length = int(input("length"))
+        area = length * width
+        print("Area of rectangle", area)
